1e6d6013e008a3e44c0c1e053c42f00120208936
Rogier <r.monshouwer@student.utwente.nl>
2020-03-15 17:12:07 +0100

    Added react-bootstrap

--- src/components/Header/NavBar.js
+++ src/components/Header/NavBar.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import {Navbar, Nav, NavDropdown} from "react-bootstrap";
 
 const NavBar = () => {
-    return <nav>
-        NavBar
-    </nav>
+    return <Navbar className="navbar-kronos" expand="lg">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <NavDropdown title="Vereninging" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                </NavDropdown>
+                <NavDropdown title="Trainingen" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                </NavDropdown>
+                <NavDropdown title="Wedstrijden" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link href="#link">Agenda</Nav.Link>
+                <Nav.Link href="#link">Uitslagen</Nav.Link>
+                <Nav.Link href="#link">Foto's</Nav.Link>
+            </Nav>
+            <Nav className="justify-content-end">
+                <NavDropdown title="Login" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                </NavDropdown>
+            </Nav>
+
+        </Navbar.Collapse>
+    </Navbar>
 };
 
 export default NavBar
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import Home from './components/App';
+
+import Home from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Home />, document.getElementById('root'));
