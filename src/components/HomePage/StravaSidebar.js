91dcdd58368a1e5a33343b51007ea0fb484ce870
Vincent Bosboom <v.bosboom@student.utwente.nl>
2020-06-07 09:25:30 +0200

    add strava sidebar

--- src/components/HomePage/StravaSidebar.js
+++ src/components/HomePage/StravaSidebar.js
@@ -4,7 +4,9 @@ import {Card} from 'react-bootstrap';
 const StravaSidebar = () => {
     return <Card className="side-panel">
         <Card.Header>Strava</Card.Header>
-        <Card.Body>Plugin</Card.Body>
+        <Card.Body>
+		<iframe title="stravabar" allowtransparency="true" frameborder ="0" height = "454" scrolling="no" src='https://www.strava.com/clubs/37924/latest-rides/962ac0041c05503286c8e79fb39538c4577229a0?show_rides=true' width= "100%"></iframe>
+		</Card.Body>
     </Card>
 };
 
